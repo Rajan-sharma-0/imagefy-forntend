a0ab6fdfec03c825ea11b5b16ac5ffe378320b56
Rajan-sharma-0 <nk800109@gmail.com>
2025-08-01 23:21:39 +0530

    improvement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='flex items-center justify-center gap-4 py-3 mt-20'>
 
-        <img src={assets.logo} alt="" width={150} />
+      <img src={assets.logo} alt="" width={150} />
       <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright Rajan | All Right reserved.</p>
 
       <div className="flex gap-2.5">
